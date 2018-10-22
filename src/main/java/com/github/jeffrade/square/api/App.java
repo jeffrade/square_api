1e42d92ad7e6720d62dbbd47c9c43228b12c4e32
Jeff Rade <jeffrade@gmail.com>
2018-10-21 20:28:49 -0500

    Adding Spring Boot for RESTful enabled Java web service

--- src/main/java/com/github/jeffrade/square/api/App.java
+++ src/main/java/com/github/jeffrade/square/api/App.java
@@ -1,8 +1,12 @@
 package com.github.jeffrade.square.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SpringApplication.run(App.class, args);
     }
 }
