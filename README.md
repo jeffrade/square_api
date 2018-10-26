9932578c0126df10b3a45e7ea1113e0d62fe04c7
Jeff Rade <jeffrade@gmail.com>
2018-10-25 21:17:10 -0500

    Adding squareup connect sdk version 2.20180918.1; Adding lombok lib; Squareup README info

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 square-api
 
+You will need to register and create your own Square Developer Account to generate your OAuth credentials. To do that, start [here](https://docs.connect.squareup.com/basics/oauth/setup#prerequisites).
+
+Before starting the app, export your credentials and secrets from Square:
+
+```
+export SQUAREUP_APP_NAME=<your-value-here>
+export SQUAREUP_API_APP_ID=<your-value-here>
+export SQUAREUP_API_ACC_TOKEN=<your-value-here>
+export SQUAREUP_API_APP_SECRET=<your-value-here>
+```
+
 Micro-service for using Square's Payments API
 
 Building:
--- src/main/java/com/github/jeffrade/square/api/controller/OAuth2Controller.java
+++ src/main/java/com/github/jeffrade/square/api/controller/OAuth2Controller.java
@@ -1,16 +1,29 @@
 package com.github.jeffrade.square.api.controller;
 
 import com.github.jeffrade.square.api.model.JsonApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 public class OAuth2Controller {
 
     @RequestMapping("/status")
     public JsonApiResponse status() {
+        log.info("Entering /status...");
         return new JsonApiResponse(200);
+    }
+
+    @RequestMapping(value="/oauth-redirect", method=RequestMethod.GET)
+    public JsonApiResponse oauthRedirect(final HttpServletRequest request) {
+        log.info("Entering /oauth-redirect...");
+        JsonApiResponse resp = new JsonApiResponse(200);
+        resp.addData("ip", request.getRemoteAddr());
+        return resp;
     }
 
 }
