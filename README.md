0babed59c7ebeee8139a126964c7b5b4d1d7f678
Jeff Rade <jeffrade@gmail.com>
2021-03-25 16:16:40 -0500

    Initial commit of Rust CLI implementation

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
-square-api
+## square-api
+
+#### Example Rust application for using Square's Payments API
 
 You will need to register and create your own Square Developer Account to generate your OAuth credentials. To do that, start [here](https://docs.connect.squareup.com/basics/oauth/setup#prerequisites).
 
-Before starting the app, export your credentials and secrets from Square:
-
+Be sure to export your credentials and secrets from Square ([prerequisites](https://developer.squareup.com/docs/testing/sandbox)):
 ```
 export SQUAREUP_APP_NAME=<your-value-here>
 export SQUAREUP_API_APP_ID=<your-value-here>
 export SQUAREUP_API_ACC_TOKEN=<your-value-here>
 export SQUAREUP_API_APP_SECRET=<your-value-here>
+export SQUAREUP_OAUTH_AUTH_REQUEST=https://connect.squareupsandbox.com/oauth2/authorize?client_id=
 ```
 
-Micro-service for using Square's Payments API
-
-Building:
+### Building
 ```
-$ gradle clean build
+$ cargo build
 ```
 
-Running:
+### Running
+
 ```
-$ java -jar build/libs/square_api.jar 
+$ cargo run login
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+#![allow(unused_imports)]
+use clap::{App, Arg, SubCommand};
+use tiny_http::{Response, Server};
+use url::{ParseError, Url};
+
+use std::env;
+use std::process::Command;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::App::new("square_api_cli")
+        .version("0.1.0")
+        .author("Jeff Rade <jeffrade@gmail.com>")
+        .about("Simple OAuth command line tool for SquareUp")
+        .subcommand(
+            clap::SubCommand::with_name("login")
+                .about("Psuedo login to launch Square's Python app to handle OAuth callback."),
+        )
+        .get_matches();
+
+    if matches.subcommand_matches("login").is_some() {
+        println!("Login in progress...");
+        // If needed, add initial user authentication happens here,
+        // but we're requesting an authenticated web browser session instead.
+
+        println!("Starting OAuth client server...");
+        let server = Server::http("0.0.0.0:5000").unwrap();
+        let authorzie_request_url =
+            String::from(env!("SQUAREUP_OAUTH_AUTH_REQUEST")) + env!("SQUAREUP_API_APP_ID");
+        println!(
+            "Waiting for user to be authorized, visit {}",
+            authorzie_request_url
+        );
+        listen(server);
+    } else {
+        println!("You must pass a valid command!");
+    }
+}
+
+fn listen(server: Server) {
+    for request in server.incoming_requests() {
+        if request.url().starts_with("/callback?") {
+            let request_url = "http://localhost:5000".to_string() + request.url();
+            let callback_url = Url::parse(&request_url).expect("Could not parse callback url");
+            let query = callback_url
+                .query()
+                .expect("Could not parse query part of callback url");
+            if let Some(code_kv) = query.split('&').collect::<Vec<&str>>().first() {
+                if let Some(code) = code_kv.split('=').collect::<Vec<&str>>().last() {
+                    println!(
+                        "User successfully authorized, authorization code is {:?}",
+                        &code
+                    );
+                }
+            }
+        } else {
+            println!("Unexpected request made: {:?}", request.url());
+        }
+    }
 }
